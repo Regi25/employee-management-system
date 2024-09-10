035d8e4c9dfdb1b89b73ffd34331ecd0ab7b78e1
regina <reginaiuldasheva@gmail.com>
2024-09-10 18:28:20 +0200

    Completed Initial Project Setup with Component Enhancements

---
+++ src/components/app-info/app-info.js
@@ -0,0 +1,13 @@
+import "./app-info.css";
+
+const AppInfo = () => {
+  return (
+    <div className="app-info">
+      <h1>Employee Management</h1>
+      <h2>Total number of employees: </h2>
+      <h2>The bonus will be received by: </h2>
+    </div>
+  );
+};
+
+export default AppInfo;
