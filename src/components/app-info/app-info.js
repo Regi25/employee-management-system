15e3f44200509462e3164067d27386073ad53b0c
regina <reginaiuldasheva@gmail.com>
2024-09-23 19:24:12 +0200

    Employee filtering implementation and event handling

--- src/components/app-info/app-info.js
+++ src/components/app-info/app-info.js
@@ -1,11 +1,11 @@
 import "./app-info.css";
 
-const AppInfo = () => {
+const AppInfo = ({ increased, employees }) => {
   return (
     <div className="app-info">
       <h1>Employee Management</h1>
-      <h2>Total number of employees: </h2>
-      <h2>The bonus will be received by: </h2>
+      <h2>Total number of employees: {employees} </h2>
+      <h2>The bonus will be received by:{increased}</h2>
     </div>
   );
 };
--- src/components/employees-list/employees-list.js
+++ src/components/employees-list/employees-list.js
@@ -2,7 +2,7 @@ import EmployeesListItem from "../employees-list-item/employees-list-item";
 
 import "./employees-list.css";
 
-const EmployeesList = ({ data }) => {
+const EmployeesList = ({ data, onDelete, onToggleProp }) => {
   const elements = data.map((item) => {
     const { id, ...itemProps } = item;
     return (
@@ -12,6 +12,10 @@ const EmployeesList = ({ data }) => {
         // salary={item.salary}
         // increase={item.increase}
         {...itemProps}
+        onDelete={() => onDelete(id)}
+        onToggleProp={(e) =>
+          onToggleProp(id, e.currentTarget.getAttribute("data-toggle"))
+        }
       />
     );
   });
