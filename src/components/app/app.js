04ccff5a8ef0722a9c2cd4ee5fa046cae15b6aff
regina <reginaiuldasheva@gmail.com>
2024-09-24 19:33:54 +0200

    Implement Search Functionality in Application

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -17,6 +17,7 @@ class App extends Component {
         { name: "Carl W.", salary: 5000, increase: false, rise: false, id: 3 },
       ],
       term: "",
+      filter: "all",
     };
     this.maxId = 4;
   }
@@ -68,28 +69,31 @@ class App extends Component {
     this.setState({ term });
   };
 
-  // onToggleRise = (id) => {
-  //   this.setState(({ data }) => ({
-  //     data: data.map((item) => {
-  //       if (item.id === id) {
-  //         return { ...item, rise: !item.rise };
-  //       }
-  //       return item;
-  //     }),
-  //   }));
-  // };
+  filterPost = (items, filter) => {
+    switch (filter) {
+      case "rise":
+        return items.filter((item) => item.rise);
+      case "moreThen1000":
+        return items.filter((item) => item.salary > 1000);
+      default:
+        return items;
+    }
+  };
+  onFilterSelect = (filter) => {
+    this.setState({ filter });
+  };
 
   render() {
-    const { data, term } = this.state;
+    const { data, term, filter } = this.state;
     const employees = this.state.data.length;
     const increased = this.state.data.filter((item) => item.increase).length;
-    const visibleData = this.searchEmp(data, term);
+    const visibleData = this.filterPost(this.searchEmp(data, term), filter);
     return (
       <div className="app">
         <AppInfo employees={employees} increased={increased} />
         <div className="search-panel">
           <SearchPanel onUpdateSearch={this.onUpdateSearch} />
-          <AppFilter />
+          <AppFilter filter={filter} onFilterSelect={this.onFilterSelect} />
         </div>
 
         <EmployeesList
